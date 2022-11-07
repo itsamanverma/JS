092ffc244b45cd4f1d5e20f2273c58d536126e0b
itsamanverma <amanvermame786@gmail.com>
2022-11-07 18:11:21 +0530

    add prime factor function

---
+++ Functional_Program/PrimeFactor.js
@@ -0,0 +1,32 @@
+/************************************************************
+ *  Execution     :  1. default node  cmd> node ReplaceString.js
+ *                   2. if nodemon installed  cmd> nodenom ReplaceString.js
+ *
+ *  Purpose       :  Computes the prime factorization of N using brute force
+ *
+ *  @description
+ *
+ *  @file         : PrimeFactor.js
+ *  @overview     : Computes the prime factorization of N using brute force.
+ *  @module       : This is optional if expeclictly its an npm or local package.
+ *  @author       : AMAN VERMA <amanvermame786.gmail.com>
+ *  @version      : 1.0
+ *  @since        : 07-11-2022
+ *
+ *************************************************************/
+
+const utility = require('./Utility.js');
+let userInput = utility.input();/* calling the input method from utility */
+
+let primeFactor = () => {
+    userInput.question("Enter your number " , (number) => {
+        if (number > 1) {
+              utility.calPrimeFactor(number);
+              userInput.close();
+        } else {
+            console.log("Enter the valid Number to get prime factor");
+        }
+    })
+};
+
+primeFactor();
--- Functional_Program/Utility.js
+++ Functional_Program/Utility.js
@@ -91,4 +91,27 @@ module.exports = {
             console.log("Please Enter the correct Number");
         }
     },
+
+    /**
+     * @description : calculate the prime factor of number 
+     * @function : 
+     */
+    calPrimeFactor(n) {
+        while(n % 2 == 0 ) {
+            console.log(2 + " ");
+            n = Math.floor(n / 2);
+        }
+
+        for (let i = 3; i <= Math.floor(Math.sqrt(n)); i = i + 2) {
+            while (n % i == 0)
+            {
+                console.log(i + " ");
+                n = Math.floor(n / i);
+            }
+        }
+
+        if (n > 2) {
+            console.log(n + " ");            
+        }
+    }
 }
