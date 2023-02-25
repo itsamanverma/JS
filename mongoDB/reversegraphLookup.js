4c6b75d59193190f55afb504eee1e353d9c3e591
itsamanverma <amanvermame786@gmail.com>
2023-02-25 18:44:05 +0530

    reverse graphLookup aggregate

---
+++ mongoDB/reversegraphLookup.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+db.parent_reference.aggregate([
+    { $match : 
+        { name: 'Shanon' }
+    },
+    {
+        $graphLookup: {
+            from: 'parent_reference',
+            startWith: '$reports_to',
+            connentFromField: 'reports_to',
+            connentToField: '_id',
+            as: 'bosses'
+        }
+    }
+]).pretty();
